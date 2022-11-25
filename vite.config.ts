e223331addccdf62916756bb743bcaedde072708
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-11-25 15:25:19 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { resolve } from 'path';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$src: resolve('./src/'),
+			$lib: resolve('./src/lib/'),
+			$components: resolve('./src/lib/components/'),
+			$store: resolve('./src/lib/store/'),
+			$templates: resolve('./src/lib/templates/'),
+			$utils: resolve('./src/lib/utils/'),
+			$types: resolve('./src/types/'),
+			$styles: resolve('./src/styles')
+		}
+	}
 };
 
 export default config;
